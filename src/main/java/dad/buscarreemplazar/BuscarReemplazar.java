2c367de6b11eb083c6316cc63cab719c69e74b95
Jorge Torres Padrón <49475382+JorgeTorresPadron@users.noreply.github.com>
2021-10-13 19:20:30 +0200

    Cambios
    Texto formateado

--- src/main/java/dad/buscarreemplazar/BuscarReemplazar.java
+++ src/main/java/dad/buscarreemplazar/BuscarReemplazar.java
@@ -12,38 +12,38 @@ import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class BuscarReemplazar extends Application {
-	
+
 	private Label buscarLabel;
 	private Label reemplazarLabel;
 	private TextField buscarText;
 	private TextField reemplazarText;
-	
+
 	private Button buscarButton;
 	private Button reemplazarButton;
 	private Button reemplazarTodoButton;
 	private Button cerrarButton;
 	private Button ayudaButton;
-	
+
 	private CheckBox mmCheckBox;
 	private CheckBox expreCheckBox;
 	private CheckBox buscarCheckBox;
 	private CheckBox resalCheckBox;
-	
+
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		
+
 		buscarLabel = new Label("Buscar: ");
 		reemplazarLabel = new Label("Reemplazar con: ");
 		buscarText = new TextField();
 		reemplazarText = new TextField();
-		
+
 		mmCheckBox = new CheckBox("Mayúsculas y minúsculas");
 		expreCheckBox = new CheckBox("Expresión regular");
 		buscarCheckBox = new CheckBox("Buscar hacia atrás");
 		resalCheckBox = new CheckBox("Resaltar resultados");
-		
+
 		buscarButton = new Button("Buscar");
-		buscarButton.setMinWidth(120); //Para que los botones no cambien de tamaño
+		buscarButton.setMinWidth(120); // Para que los botones no cambien de tamaño
 		reemplazarButton = new Button("Reemplazar");
 		reemplazarButton.setMinWidth(120);
 		reemplazarTodoButton = new Button("Reemplazar Todo");
@@ -52,13 +52,12 @@ public class BuscarReemplazar extends Application {
 		cerrarButton.setMinWidth(120);
 		ayudaButton = new Button("Ayuda");
 		ayudaButton.setMinWidth(120);
-		
-		
+
 		GridPane panel = new GridPane();
 		panel.add(buscarLabel, 0, 0);
-		panel.add(buscarText, 1, 0, 2, 1); //1,0 posicion 2,1 para indicar que ocupa 2 columnas y 1 fila
+		panel.add(buscarText, 1, 0, 2, 1); // 1,0 posicion 2,1 para indicar que ocupa 2 columnas y 1 fila
 		panel.add(reemplazarLabel, 0, 1);
-		panel.add(reemplazarText, 1, 1, 2, 1);  //1,0 posicion 2,1 para indicar que ocupa 2 columnas y 1 fila
+		panel.add(reemplazarText, 1, 1, 2, 1); // 1,0 posicion 2,1 para indicar que ocupa 2 columnas y 1 fila
 		panel.add(mmCheckBox, 1, 2);
 		panel.add(buscarCheckBox, 2, 2);
 		panel.add(expreCheckBox, 1, 3);
@@ -70,12 +69,12 @@ public class BuscarReemplazar extends Application {
 		panel.add(ayudaButton, 3, 4);
 		panel.setHgap(5);
 		panel.setVgap(5);
-		
+
 		BorderPane root = new BorderPane();
 		root.setCenter(panel);
 		root.setPadding(new Insets(5));
-		
-		Scene scene= new Scene(root,480,250);
+
+		Scene scene = new Scene(root, 480, 250);
 		primaryStage.setTitle("Buscar y reemplazar");
 		primaryStage.setScene(scene);
 		primaryStage.show();
